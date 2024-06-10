d333aafe8e106b7801655e2108aeb49e8c1dd686
Rohan Chopra <git@rchopra.in>
2024-06-10 13:51:49 -0400

    Complete hw3 with mlflow integration

--- mlops/mlops/homework_03/data_exporters/eccentric_herald.py
+++ mlops/mlops/homework_03/data_exporters/eccentric_herald.py
@@ -1,6 +1,11 @@
+import mlflow
+import pickle
+
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
+mlflow.set_tracking_uri("http://mlflow:5000")
+mlflow.set_experiment("nyc-taxi-hw-3")
 
 @data_exporter
 def export_data(data, *args, **kwargs):
@@ -15,5 +20,13 @@ def export_data(data, *args, **kwargs):
         Optionally return any object and it'll be logged and
         displayed when inspecting the block run.
     """
+    dv = data[-1]
+    with mlflow.start_run():
+        with open('lin_reg_dv.bin', 'wb') as f_out:
+            pickle.dump(dv, f_out)
+
+        mlflow.sklearn.log_model(data[0], "lr_model")
+        mlflow.log_artifact(local_path="lin_reg_dv.bin", artifact_path="dv_pickle")
+
     return data[0].intercept_
 
